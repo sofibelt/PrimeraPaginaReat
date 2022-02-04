9bc4681b5bce15d8661e180ed9d9869d46ab5a7c
Ana Sofia Beltran <anasobeltran@gmail.com>
2022-02-04 12:22:33 -0500

    añadir imagenes

--- src/Imagenes.js
+++ src/Imagenes.js
@@ -1,8 +1,51 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Imagenes(){
   return(
-    <div className="col-md-8">
-      <img className="img-fluid" src={require('./img/kioto.jpg')} width="400px" />
+    <div class="offset-2">
+
+      <div class="row ">
+        <div  class="col-2 ">
+          <img src={require('./img/kioto.jpg')} width="300px" />
+        </div>
+        <div class="col-10 ">
+          <div class="row offset-2">
+              <div  class="col-2 ">
+                <img  src={require('./img/kioto.jpg')} width="100px" />
+              </div>
+              <div class="col-2">
+                <img  src={require('./img/kioto.jpg')} width="100px" />
+              </div>
+              <div class="col-2">
+                <img  src={require('./img/kioto.jpg')} width="100px" />
+              </div>
+            </div>
+            <div class="row offset-2">
+                <div  class="col-2 ">
+                  <img  src={require('./img/kioto.jpg')} width="100px" />
+                </div>
+                <div class="col-2">
+                  <img  src={require('./img/kioto.jpg')} width="100px" />
+                </div>
+                <div class="col-2">
+                  <img  src={require('./img/kioto.jpg')} width="100px" />
+                </div>
+              </div>
+        </div>
+
+
+      </div>
     </div>
+    /*
+    <div>
+      <div className="col-lg-2 offset-2 bg-warning">
+        <img class="rounded float-left" src={require('./img/kioto.jpg')} width="100px" />
+      </div>
+      <div className="col-lg-2 offset-2 bg-warning">
+        <img class="rounded float-left" src={require('./img/kioto.jpg')} width="100px" />
+      </div>
+    </div>
+    */
   );
 }
 
