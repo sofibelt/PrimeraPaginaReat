505672092cd5485087ad496b206ee36215663e52
Ana Sofia Beltran <anasobeltran@gmail.com>
2022-02-03 20:03:03 -0500

    inicial

---
+++ src/Imagenes.js
@@ -0,0 +1,9 @@
+function Imagenes(){
+  return(
+    <div className="col-md-8">
+      <img className="img-fluid" src={require('./img/kioto.jpg')} width="400px" />
+    </div>
+  );
+}
+
+export default Imagenes;
